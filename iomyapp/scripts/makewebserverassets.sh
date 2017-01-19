582a2e1863d1fa8d4e4e571f40dedd9937948eaa
Matthew Stapleton <matthew@capsicumcorp.com>
2017-01-19 11:45:03 +1000

    iomyapp: Added support in the asset creation scripts for adding a demo database to the assets

--- iomyapp/scripts/makewebserverassets.sh
+++ iomyapp/scripts/makewebserverassets.sh
@@ -133,6 +133,14 @@ else
 	fi
 fi
 
+if [ -f "${basedir}/download/${DEMODATABASEPKG}" ] ; then
+  echo "Extracting MySQL demo database from ${DEMODATABASETITLE}-${DEMODATABASEVER}"
+	xz -dc "${basedir}/download/${DEMODATABASEPKG}" | tar x
+	if [ $? != 0 ] ; then
+	  exit 1
+	fi
+fi
+
 # Copy the iomyweb files from the iomyweb folder
 echo "Copying iomyweb files from the iomyweb folder"
 bash ${basedir}/scriptlib/copyiomyweb.sh "${basedir}/../../iomyweb" "${basedir}/assetstmp"
