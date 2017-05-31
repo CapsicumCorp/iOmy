d20b07d65acf975d179d486e65ffcc43b02e9a10
Matthew Stapleton <matthew@capsicumcorp.com>
2017-05-31 15:29:48 +1000

    App: Rewrote the first run external storage detection
    The first run external storage detection had some problems with some
    devices including a null pointer exception which has now been fixed

--- iomyapp/app/src/main/java/com/capsicumcorp/iomy/apps/iomy/FirstRunDatabaseStorageLocationPage.java
+++ iomyapp/app/src/main/java/com/capsicumcorp/iomy/apps/iomy/FirstRunDatabaseStorageLocationPage.java
@@ -1,11 +1,31 @@
+/*
+Title: Database Storage Location Page for First Run Wizard
+Author: Matthew Stapleton (Capsicum Corporation) <matthew@capsicumcorp.com>
+Description: Prompts the user to select which storage device to use to store the database to
+Copyright: Capsicum Corporation 2017
+
+This file is part of iOmy.
+
+iOmy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+iOmy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with iOmy. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.capsicumcorp.iomy.apps.iomy;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v4.os.EnvironmentCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -14,8 +34,6 @@ import android.widget.RadioGroup;
 import java.io.File;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
-
-import static java.lang.Math.round;
 
 public class FirstRunDatabaseStorageLocationPage extends AppCompatActivity {
     private InstallWizard installWizard = Constants.installWizard;
@@ -54,12 +72,15 @@ public class FirstRunDatabaseStorageLocationPage extends AppCompatActivity {
             //Pre Android 4.4 (API 19) External Storage Radio Button
             if (!Environment.isExternalStorageEmulated()) {
                 //Only show External Storage if it is on a different partition to internal storage
-                RadioButton radioButtonExternalStorage = new RadioButton(this);
-                File externalStorageFile = this.getExternalFilesDir(null);
-                String externalStorageFreeSpaceStr = df.format(((float) externalStorageFile.getFreeSpace()) / 1024.0 / 1024.0 / 1024.0);
-                radioButtonExternalStorage.setText("SD Card, " + externalStorageFreeSpaceStr + " GB free");
-                radioButtonExternalStorage.setId(R.id.storage_location_external);
-                rgp.addView(radioButtonExternalStorage);
+                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                    //Only show External Storage it is available and writable
+                    RadioButton radioButtonExternalStorage = new RadioButton(this);
+                    File externalStorageFile = this.getExternalFilesDir(null);
+                    String externalStorageFreeSpaceStr = df.format(((float) externalStorageFile.getFreeSpace()) / 1024.0 / 1024.0 / 1024.0);
+                    radioButtonExternalStorage.setText("SD Card, " + externalStorageFreeSpaceStr + " GB free");
+                    radioButtonExternalStorage.setId(R.id.storage_location_external);
+                    rgp.addView(radioButtonExternalStorage);
+                }
             }
         } else {
             File[] externalStorageFiles=this.getExternalFilesDirs(null);
@@ -68,23 +89,41 @@ public class FirstRunDatabaseStorageLocationPage extends AppCompatActivity {
 
             int i=0;
             for(File externalStorageFile : externalStorageFiles) {
-                if (!EnvironmentCompat.getStorageState(externalStorageFile).equals(Environment.MEDIA_MOUNTED)) {
-                    //Only provide options for media that can be written to
-                    //Log.println(Log.INFO, "IOMY", "DEBUG Can't write to storage: "+externalStoragePath.getPath() + " "+EnvironmentCompat.getStorageState(externalStoragePath));
+                if (externalStorageFile==null) {
+                    //Some devices will return external storage slots that don't actually exist
+                    //Log.println(Log.INFO, "IOMY", "DEBUG External storage slot doesn't exist");
                     continue;
                 }
-                //Only show External Storage if it is on a different partition to internal storage (emulated)
-                if (externalStorageFile.getPath().equals(this.getExternalFilesDir(null).getPath())) {
-                    //Log.println(Log.INFO, "IOMY", "DEBUG Found first external storage: "+externalStoragePath.getPath());
-                    if (Environment.isExternalStorageEmulated()) {
-                        //Log.println(Log.INFO, "IOMY", "DEBUG First external storage is emulated: "+externalStoragePath.getPath());
+                if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+                    if (!Environment.getStorageState(externalStorageFile).equals(Environment.MEDIA_MOUNTED)) {
+                        //Only show External Storage if is available and writable
+                        //Log.println(Log.INFO, "IOMY", "DEBUG 19 Can't write to other external storage: "+externalStorageFile.getPath() + " "+Environment.getStorageState(externalStorageFile));
                         continue;
                     }
                 }
                 if (Build.VERSION.SDK_INT >= 21) {
                     //Android Lollipop (API 21) adds ability to check any external storage if it is emulated
                     if (Environment.isExternalStorageEmulated(externalStorageFile)) {
-                        //Log.println(Log.INFO, "IOMY", "DEBUG Other external storage is emulated: "+externalStoragePath.getPath());
+                        //Log.println(Log.INFO, "IOMY", "DEBUG 21 Other external storage is emulated: "+externalStorageFile.getPath());
+                        continue;
+                    }
+                    if (!Environment.getExternalStorageState(externalStorageFile).equals(Environment.MEDIA_MOUNTED)) {
+                        //Only provide options for media that can be written to
+                        //Log.println(Log.INFO, "IOMY", "DEBUG 21 Can't write to other external storage: "+externalStorageFile.getPath() + " "+Environment.getExternalStorageState(externalStorageFile));
+                        continue;
+                    }
+                }
+                if (externalStorageFile.getPath().equals(this.getExternalFilesDir(null).getPath())) {
+                    //Primary external storage has dedicated methods for determining info
+                    //Log.println(Log.INFO, "IOMY", "DEBUG Found first external storage: "+externalStorageFile.getPath());
+                    if (Environment.isExternalStorageEmulated()) {
+                        //Only show External Storage if it is on a different partition to internal storage (emulated)
+                        //Log.println(Log.INFO, "IOMY", "DEBUG First external storage is emulated: "+externalStorageFile.getPath());
+                        continue;
+                    }
+                    if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                        //Only show External Storage if is available and writable
+                        //Log.println(Log.INFO, "IOMY", "DEBUG Can't write to First external storage: "+externalStorageFile.getPath() + " "+Environment.getExternalStorageState());
                         continue;
                     }
                 }
