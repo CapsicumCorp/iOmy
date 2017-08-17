81f3d16bcddc6460dc2ef83533f000fefbd83b33
Brent Jarmaine <brenton@capsicumcorp.com>
2017-08-17 15:26:41 +1000

    New function to create the select box listing all rooms.
    * Has the option to show the 'Unassigned' 'room'. Used for room
    permissions.
    * Has the option to have the 'All Rooms' option. Again used for room
    permissions.

--- iomyweb/ui/mobile/util/widgets/getRoomSelector.js
+++ iomyweb/ui/mobile/util/widgets/getRoomSelector.js
@@ -32,51 +32,162 @@ $.extend(IOMy.widgets,{
      * receive the ID of the room that is currently selected if changing from one room
      * to another.
      * 
-     * @param {string} sId				ID for the select box.
-     * @param {string} sPremiseId		ID of the given premise.
-     * @param {Number} iRoomId			(optional) ID of the room currently set.
-     * @returns {sap.m.Select}			Select box with the rooms in a given premise.
-	 * @throws NoRoomsFoundException	
+     * @param {string} sId                ID for the select box.
+     * @param {string} sPremiseId         ID of the given premise.
+     * @param {Number} iRoomId            (optional) ID of the room currently set.
+     * @returns {sap.m.Select}            Select box with the rooms in a given premise.
+     * @throws NoRoomsFoundException    
      */
-    getRoomSelector : function (sId, sPremiseId, iRoomId) {
+    selectBoxRoom : function (mSettings) {
+        var bError              = false;
+        var aErrorMessages      = [];
+        var aItems              = [];
+        var aFirstItem          = [];
+        var sId;
+        var sPremiseId;
+        var iRoomId;
+        var bIncludeUnassigned;
+        var bAddAllRoomOption;
+        var oSBox;
+        
+        if (mSettings !== undefined) {
+            
+            if (mSettings.premiseID === undefined || mSettings.premiseID === null) {
+                
+                //--------------------------------------------------------------------//
+                // Fetch the premise ID.
+                // 
+                // NOTE: This currently selects the first premise in the list, which is
+                // normally the only one in the list. When we start adding support for
+                // multiple premises, this code will need to be redone.
+                //--------------------------------------------------------------------//
+                $.each(IOMy.common.PremiseList, function (sI, mPremise) {
+                    sPremiseId = sI;
+                    return false;
+                });
+                
+            } else {
+                sPremiseId = mSettings.premiseID;
+                
+                if (isNaN(sPremiseId)) {
+                    if (sPremiseId.charAt(0) !== '_') {
+                        bError = true;
+                        aErrorMessages = "Premise ID is not in a valid format."
+                    }
+                } else {
+                    sPremiseId = "_" + sPremiseId;
+                }
+            }
+            
+            if (mSettings.id === undefined || mSettings.id === null) {
+                sId = null;
+            } else {
+                sId = mSettings.id;
+            }
+            
+            if (mSettings.roomID === undefined || mSettings.roomID === null) {
+                iRoomId = null;
+            } else {
+                iRoomId = mSettings.roomID;
+            }
+            
+            if (mSettings.showUnassigned === undefined || mSettings.showUnassigned === null) {
+                bIncludeUnassigned = false;
+            } else {
+                bIncludeUnassigned = mSettings.showUnassigned;
+            }
+            
+            if (mSettings.showAllRoomOption === undefined || mSettings.showAllRoomOption === null) {
+                bAddAllRoomOption = false;
+            } else {
+                bAddAllRoomOption = mSettings.showAllRoomOption;
+            }
+            
+            if (bError) {
+                throw new IllegalArgumentException(aErrorMessages.join("\n"));
+            }
+            
+        } else {
+            //--------------------------------------------------------------------//
+            // Fetch the premise ID.
+            // 
+            // NOTE: This currently selects the first premise in the list, which is
+            // normally the only one in the list. When we start adding support for
+            // multiple premises, this code will need to be redone.
+            //--------------------------------------------------------------------//
+            $.each(IOMy.common.PremiseList, function (sI, mPremise) {
+                sPremiseId = sI;
+                return false;
+            });
+            
+            iRoomId = null;
+            bIncludeUnassigned = false;
+            bAddAllRoomOption = false;
+        }
+        
         try {
             //====================================================================//
             // Declare Variables                                                  //
             //====================================================================//
             var iRoomsCounted = 0;
-			var bHasUnassignedRoom = false;
+            var bHasUnassignedRoom = false;
+            
+            if (bIncludeUnassigned === undefined || bIncludeUnassigned === null) {
+                bIncludeUnassigned = false;
+            }
 
             //====================================================================//
             // Create the Select Box                                              //
             //====================================================================//
-			if (sap.ui.getCore().byId(sId) !== undefined) {
-				sap.ui.getCore().byId(sId).destroy();
-			}
-			
+            if (sap.ui.getCore().byId(sId) !== undefined) {
+                sap.ui.getCore().byId(sId).destroy();
+            }
+            
             if (IOMy.common.RoomsList[sPremiseId] !== undefined) {
-                var oSBox = new sap.m.Select(sId,{
-                    "width" : "100%"
-                }).addStyleClass("width100Percent");
+                
+                if (bAddAllRoomOption) {
+                    aFirstItem.push(
+                        new sap.ui.core.Item({
+                            "text" : "All Rooms",
+                            "key" : 0
+                        })
+                    );
+                }
                 
                 $.each(IOMy.common.RoomsList[sPremiseId],function(sIndex,aRoom) {
                     //-- Verify that the Premise has rooms, other than the pseudo-room Unassigned --//
                     if( sIndex!==undefined && sIndex!==null && aRoom!==undefined && aRoom!==null)
-					{
-						if (aRoom.RoomId === 1 && aRoom.RoomName === "Unassigned") {
-							bHasUnassignedRoom = true;
-							
-						} else {
-							oSBox.addItem(
-								new sap.ui.core.Item({
-									"text" : aRoom.RoomName,
-									"key" : aRoom.RoomId
-								})
-							);
-						
-							iRoomsCounted++;
-						}	
+                    {
+                        if (aRoom.RoomId === 1 && aRoom.RoomName === "Unassigned" && !bIncludeUnassigned) {
+                            bHasUnassignedRoom = true;
+                            
+                        } else {
+                            aItems.push(
+                                new sap.ui.core.Item({
+                                    "text" : aRoom.RoomName,
+                                    "key" : aRoom.RoomId
+                                })
+                            );
+                        
+                            iRoomsCounted++;
+                        }
                     }
                 });
+                
+                aItems = aFirstItem.concat(aItems);
+                
+                if (sId !== null) {
+                    oSBox = new sap.m.Select(sId,{
+                        "width" : "100%",
+                        "items" : aItems
+                    }).addStyleClass("width100Percent");
+
+                } else {
+                    oSBox = new sap.m.Select({
+                        "width" : "100%",
+                        "items" : aItems
+                    }).addStyleClass("width100Percent");
+                }
                 
                 if (iRoomsCounted > 0) {
                     if (iRoomId !== undefined && iRoomId !== null) {
@@ -87,11 +198,11 @@ $.extend(IOMy.widgets,{
                     
                     return oSBox;
                 } else {
-					if (bHasUnassignedRoom) {
-						oSBox.setVisible(false);
-					} else {
-						throw new NoRoomsFoundException();
-					}
+                    if (bHasUnassignedRoom) {
+                        oSBox.setVisible(false);
+                    } else {
+                        throw new NoRoomsFoundException();
+                    }
                 }
                 
             } else {
@@ -99,9 +210,31 @@ $.extend(IOMy.widgets,{
             }
 
         } catch (e) {
-			e.message = "Error in IOMy.widgets.getRoomSelector(): "+e.message;
+            e.message = "Error in IOMy.widgets.getRoomSelector(): "+e.message;
             throw e;
         }
+    },
+    
+    getRoomSelector : function (sId, sPremiseId, iRoomId, bIncludeUnassigned, bIncludeAllRoomOption) {
+        if (iRoomId === undefined) {
+            iRoomId = false;
+        }
+        
+        if (bIncludeUnassigned === undefined || bIncludeUnassigned === null) {
+            bIncludeUnassigned = false;
+        }
+        
+        if (bIncludeAllRoomOption === undefined || bIncludeAllRoomOption === null) {
+            bIncludeAllRoomOption = false;
+        }
+        
+        return this.selectBoxRoom({
+            id : sId,
+            premiseID : sPremiseId,
+            roomID : iRoomId,
+            showUnassigned : bIncludeUnassigned,
+            showAllRoomOption : bIncludeAllRoomOption
+        });
     }
     
 });
