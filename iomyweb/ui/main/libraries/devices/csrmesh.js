926c8e5686862db6146d7a20d78b8d447e2e7b87
Brent Jarmaine <brenton@capsicumcorp.com>
2017-10-26 08:41:15 +1000

    UI: New CSR Mesh Module

---
+++ iomyweb/ui/main/libraries/devices/csrmesh.js
@@ -0,0 +1,35 @@
+/*
+Title: CSR Bluetooth Mesh Device Module
+Author: Brent Jarmaine (Capsicum Corporation) <brenton@capsicumcorp.com>
+Description: Provides the information for a CSR Mesh device.
+Copyright: Capsicum Corporation 2017
+
+This file is part of iOmy.
+
+iOmy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+iOmy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with iOmy. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+$.sap.declare("IomyRe.devices.csrmesh",true);
+IomyRe.devices.csrmesh = new sap.ui.base.Object();
+
+$.extend(IomyRe.devices.csrmesh,{
+    
+    LinkTypeId        : 5,
+    ThingTypeId       : 13,
+    
+    RSHue           : 3901,
+    RSSaturation    : 3902,
+    RSBrightness    : 3903,
+});
