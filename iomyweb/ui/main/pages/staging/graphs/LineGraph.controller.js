1d614f82784a1f230a2513596e03f0b2ed5b63ef
Capsicum-Andrew <andrew@capsicumcorp.com>
2017-12-11 13:57:04 +1000

    UI: Swapped the UI over to using
    == Change Log ==
    * On the Line Graphs page over from using the average aggregation to
    just the plain values.

--- iomyweb/ui/main/pages/staging/graphs/LineGraph.controller.js
+++ iomyweb/ui/main/pages/staging/graphs/LineGraph.controller.js
@@ -115,7 +115,7 @@ sap.ui.controller("pages.staging.graphs.LineGraph", {
 			url:       IomyRe.apiphp.APILocation("graph"),
 			data:      {
 				"Mode": "GraphLine",
-				"Data": "{\"Type\":\"NormalAvg\",\"IOId\":"+oController.iIOId+"}",
+				"Data": "{\"Type\":\"Normal\",\"IOId\":"+oController.iIOId+"}",
 				"StartUTS": iStartUTS,
 				"EndUTS":   iEndUTS,
 				"Points":   100
